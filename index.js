e4f736dac4978f968d60956030d116a3552bc602
g vijesh kumar <gvijeshkumar@gmail.com>
2023-07-11 00:27:49 +0530

    updated cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 5000;  
 
 app.use(cors({
-    origin: "https://couponhuntter.vercel.app/",
+    origin: "*",
 }));
 app.use(express.json())
 
